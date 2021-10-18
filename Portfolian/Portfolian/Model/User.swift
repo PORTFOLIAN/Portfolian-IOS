2d8fa59266f5f4251a56adcc4d55f887e36fdbe6
yi-sang <westernize@naver.com>
2021-10-19 08:40:25 +0900

    Feat: 탭바까지의 커밋

---
+++ Portfolian/Portfolian/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Portfolian
+//
+//  Created by 이상현 on 2021/10/18.
+//
+
+import Foundation
+class User {
+    var flag: Bool = false
+    static let shared = User()
+    
+    
+}
